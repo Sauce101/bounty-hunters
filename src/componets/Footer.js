e6966af94bbc0150842dd1483a8a8dc77f7dfdc1
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2022-09-13 21:58:39 -0700

    added my first and last name to footer

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -1,11 +1,8 @@
 function Footer() {
   return (
     <footer className="bg-gray-200 text-center lg:text-left mt-auto">
-      <div className="text-gray-700 text-center p-4 font-exo font-normal">
-        © 2022 Copyright: Mikey
-        {/* <a className="text-gray-800" href="https://tailwind-elements.com/">
-          Tailwind Elements
-        </a> */}
+      <div className="text-gray-700 text-center p-3 font-exo font-normal">
+        © 2022 Michael Saucedo
       </div>
     </footer>
   )
