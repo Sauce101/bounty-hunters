d7f17a56226febfd186a13e7b7ec5915fb834813
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2022-09-25 00:50:42 -0700

    adjusted footer to fixed bottom-0

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-gray-200 text-center lg:text-left mt-auto sticky bottom-0">
+    <footer className="bg-gray-200 text-center lg:text-left mt-auto fixed bottom-0 w-full">
       <div className="text-gray-700 text-center p-3 font-exo font-normal">
         © 2022 Michael Saucedo
       </div>
