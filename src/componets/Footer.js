1fce18df958ab94affe291f66c6e08ff38a93eed
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2022-09-25 01:24:55 -0700

    adjusted footer to top-screen

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-gray-200 text-center lg:text-left mt-auto sticky bottom-0 justify-self-end">
+    <footer className="bg-gray-200 text-center lg:text-left mt-auto sticky top-screen">
       <div className="text-gray-700 text-center p-3 font-exo font-normal">
         © 2022 Michael Saucedo
       </div>
