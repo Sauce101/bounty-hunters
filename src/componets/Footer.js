a89d319b8040f79ddfb5ff8cda4acdf645bd4231
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2022-09-25 02:17:31 -0700

    ok its bedtime and went fixed with 0 bottom r and l

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-gray-200 text-center lg:text-left mt-auto sticky top-screen bottom-0">
+    <footer className="bg-gray-200 text-center lg:text-left mt-auto fixed left-0 right0 bottom-0">
       <div className="text-gray-700 text-center p-3 font-exo font-normal">
         © 2022 Michael Saucedo
       </div>
